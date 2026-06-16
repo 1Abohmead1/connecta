db4d2278e97a6ea913c55e4913d6c89ad8713841
Ahmed <ahmedgaming8y7y@gmail.com>
2026-06-17 02:59:52 +0300

    getDb function fix

--- db.py
+++ db.py
@@ -3,6 +3,6 @@ from psycopg2.extras import RealDictCursor
 import os
 
 def getDb():
-  db = psycopg2.connect(os.environ.get('DATABASE_URL'), cursor_factory=RealDictCursor)
-  conn = db.cursor(cursor_factory=RealDictCursor)
-  return conn ,db
+  conn = psycopg2.connect(os.environ.get('DATABASE_URL'), cursor_factory=RealDictCursor)
+  db = conn.cursor(cursor_factory=RealDictCursor)
+  return conn, db
