e8e56b0ce8d6218c4a7738ef59c16ae6319d43d8
Ahmed <ahmedgaming8y7y@gmail.com>
2026-06-18 16:31:11 +0300

    connecta

--- upload.py
+++ upload.py
@@ -12,5 +12,9 @@ def allowed_file(filename, ALLOWED_EXTENSIONS):
   return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
 def save_file(file):
-  result = cloudinary.uploader.upload(file)
+  ext = file.filename.rsplit('.', 1)[1].lower()
+  if ext in ['mp4', 'mkv']:
+    result = cloudinary.uploader.upload(file, resource_type='video')
+  else:
+    result = cloudinary.uploader.upload(file, resource_type='image')
   return result['secure_url']
